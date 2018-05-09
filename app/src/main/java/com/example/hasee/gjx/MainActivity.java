73614597dbdf4d39aa6f743821f2b8e72ce12997
13701125264@163.com <13701125264@163.com>
2018-05-09 19:13:47 +0800

    创建个方法

--- app/src/main/java/com/example/hasee/gjx/MainActivity.java
+++ app/src/main/java/com/example/hasee/gjx/MainActivity.java
@@ -12,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.e("sdsad","sdasdads");
+        intn();
+    }
+
+    private void intn() {
+        
     }
 }
